7e564385c8c3d0ee0fa8b9702efebbc9afa93e2a
taijuten <taijuten@googlemail.com>
2017-01-08 20:44:16 +0000

    remove duplicated code

--- src/controllers/categories/delete.js
+++ src/controllers/categories/delete.js
@@ -1,5 +1,5 @@
-const { category, product, image } = require('../../models');
-const { requestHelpers } = require('../../utils');
+const categoryGet = require('./get');
+const { category } = require('../../models');
 const Boom = require('boom');
 
 module.exports = (request, reply) => {
@@ -11,27 +11,7 @@ module.exports = (request, reply) => {
     .then(categoryToDestroy => {
       categoryToDestroy.destroy()
         .then(destroyedProduct => {
-          const includes = requestHelpers.parseIncludes({ product, image }, request.query.include);
-          const orderProperty = request.query.orderBy || 'id';
-          const order = (request.query.orderBy) ? request.query.orderBy.map((orderProp, index) => {
-            let orderArray = [orderProp];
-            if (request.query.orderDirection && request.query.orderDirection[index]) {
-              orderArray.push(request.query.orderDirection[index])
-            }
-            return orderArray;
-          }) : [['id']];
-          category.findAll({
-            include: includes,
-            limit: request.query.limit || 25,
-            offset: request.query.offset || 0,
-            order: order
-          })
-            .then(categoryList => {
-              reply(categoryList);
-            })
-            .catch(err => {
-              reply(Boom.badImplementation('Could not retrieve categories'));
-            });
+          categoryGet(request, reply);
         })
         .catch(err => {
           reply(Boom.badImplementation('The category could not be destroyed'));
--- src/controllers/products/delete.js
+++ src/controllers/products/delete.js
@@ -1,5 +1,5 @@
-const { category, image, product } = require('../../models');
-const { requestHelpers } = require('../../utils');
+const { product } = require('../../models');
+const productGet = require('./get');
 const Boom = require('boom');
 
 module.exports = (request, reply) => {
@@ -11,33 +11,7 @@ module.exports = (request, reply) => {
     .then(productToDestroy => {
       productToDestroy.destroy()
         .then(destroyedProduct => {
-          const includes = requestHelpers.parseIncludes({ category, image }, request.query.include);
-          const order = requestHelpers.parseOrder(request.query.orderBy, request.query.orderDirection);
-          request.server.auth.test('jwt', request, (err, credentials) => {
-            const paranoid = (err || request.query.includeDeleted !== 'true');
-
-            let whereClauses = {};
-            if (err || request.query.includeDrafts !== 'true') {
-              whereClauses.publishedAt = {
-                $lte: new Date()
-              }
-            }
-
-            product.findAll({
-              where: whereClauses,
-              include: includes,
-              limit: request.query.limit || 25,
-              offset: request.query.offset || 0,
-              order: order,
-              paranoid: paranoid
-            })
-              .then(productList => {
-                reply(productList);
-              })
-              .catch(err => {
-                reply(Boom.badImplementation('Could not retrieve products'));
-              });
-          });
+          productGet(request, reply);
         })
         .catch(err => {
           reply(Boom.badImplementation('The product could not be destroyed'));
