9a93b1f30243db339e67fe965568f9bd9bb08544
Ralph Lawrence <taijuten@googlemail.com>
2017-01-12 22:11:11 +0000

    Allow drafts on products (#6)
    * allow drafts to be included within product lists
    * allow images to be submitted as part of product post, and return image meta with resourceLink as part of response

--- src/controllers/products/create.js
+++ src/controllers/products/create.js
@@ -1,20 +1,82 @@
-const { product } = require('../../models');
+const { image, product } = require('../../models');
+const { file } = require('../../utils');
+const path = require('path');
 const Boom = require('boom');
+const Promise = require('bluebird');
+const appDir = path.dirname(require.main.filename);
+
+const addImages = function(request, originalProduct) {
+  return new Promise(function(resolve, reject) {
+
+    const images = request.payload.images;
+    if (!images || images.length === 0) {
+      resolve(originalProduct);
+    }
+
+    const incrementUploadedFiles = function() {
+      uploadedFiles++;
+      if (uploadedFiles === images.length) {
+        product.findOne({
+          include: [{
+            model: image,
+            as: 'image'
+          }],
+          where: { id: originalProduct.id }
+        }).then(productWithImages => {
+          resolve(productWithImages);
+        }).catch(err => reject(Boom.badImplementation('Could not retrieve saved product' + err)));
+      }
+    };
+
+    let uploadedFiles = 0;
+    for (const imageUpload of images) {
+      image.create().then(createdImage => {
+        createdImage.addProduct(originalProduct.id)
+          .then(linkedProduct => {
+            const name = `${createdImage.id}-original`;
+            const filepath = appDir + "/storage/" + name;
+            file.save(filepath, imageUpload, function(err) {
+              if (err) {
+                reject(Boom.badImplementation(err));
+              } else {
+                incrementUploadedFiles();
+              }
+            });
+          })
+          .catch(err => {
+            reject(Boom.badImplementation('Could not add products to image ' + createdImage.id, err));
+          });
+        }).catch(err => console.error(err));
+    }
+  });
+};
+
+const addCategories = function(request, originalProduct) {
+  return new Promise(function(resolve, reject) {
+    const categories = request.payload.categoryIds;
+    if (!categories) {
+        resolve(originalProduct);
+    }
+    originalProduct.addCategory(categories)
+      .then(linkedProduct => {
+        resolve(originalProduct);
+      })
+      .catch(err => {
+        reject(Boom.badImplementation('Could not add categories to product ' + originalProduct.id));
+      });
+  });
+}
 
 module.exports = (request, reply) => {
   product.create(request.payload)
     .then(createdProduct => {
-      if (request.payload.categoryIds) {
-        createdProduct.addCategories(request.payload.categoryIds)
-          .then(linkedProduct => {
-            reply(createdProduct);
-          })
-          .catch(err => {
-            reply(Boom.badImplementation('Could not add categories to product ' + createdProduct.id));
-          });
-      } else {
-        reply(createdProduct);
-      }
+      // add images
+      createdProduct = addImages(request, createdProduct)
+        // add categories
+        .then( addCategories.bind(null, request) )
+        .then( function(res) {
+          reply(res);
+        });
     })
     .catch(err => {
       reply(Boom.badImplementation('Could not create product'));
