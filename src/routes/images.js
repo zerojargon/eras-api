46ab5f083a15e70cc70d9ccfbea848bc5f3f13b4
Ralph Lawrence <taijuten@googlemail.com>
2017-01-15 15:00:05 +0000

    increase image upload size limit (#7)

--- src/routes/images.js
+++ src/routes/images.js
@@ -24,7 +24,8 @@ module.exports = [
       payload: {
         output: 'stream',
         parse: true,
-        allow: 'multipart/form-data'
+        allow: 'multipart/form-data',
+        maxBytes: 50000000
       },
       validate: {
         payload: Joi.object().keys({
